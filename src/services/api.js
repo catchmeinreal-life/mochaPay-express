2dc57110ee6d9e7cd368eb5cc677403c705f8728
eric matutu <ericmatutu125@gmail.com>
2025-07-26 21:59:46 +0300

    backend api: - link btwn client and the backend - currently connection is not connect finding difficulty

---
+++ src/services/api.js
@@ -0,0 +1,48 @@
+console.log("Base URL:", import.meta.env.VITE_MOCHA_API_URL);
+
+import axios from "axios";
+
+const MochaApi = axios.create({
+  baseURL: import.meta.env.VITE_MOCHA_API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+MochaApi.interceptors.request.use(
+    (config) => {
+        //add tokens from localStorage
+        const token = localStorage.getItem('token');
+        if(token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        console.error('error making request:', error.message);
+        return Promise.reject(error.message);
+    }
+);
+
+MochaApi.interceptors.response.use(
+    (response) => {
+        return response;
+    },
+    (error) => {
+        console.error('got an error while waiting for the response', error.message);
+        return Promise.reject(error);
+    }
+)
+
+/**
+ * send and receive mocha 
+ * new user onBoarding
+ */
+
+export const authService = {
+    //get greeting message from the backend
+    greetUser: async () => {
+        const response = await MochaApi.get('/api/auth/message');
+        return response.data;
+    },
+}
