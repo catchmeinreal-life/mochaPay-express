f206335127ab827acfa4ebad8b33c7f0df5c04b7
eric matutu <ericmatutu125@gmail.com>
2025-07-26 23:58:33 +0300

    created the navbar and css file

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,48 @@
+import { Link, useLocation } from "react-router-dom";
+import React from "react";
+
+import '../styles/components/navbar.css'
+
+function NavBar() {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+  const location = useLocation();
+
+  const isMainPage = location.pathname === '/';
+  
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+    return (
+        <nav>
+            <div className="navbar">
+                <Link to="/" className="navbar-brand">MochaPay</Link>
+                
+                <button className="navbar-toggle" onClick={toggleMenu}>
+                    ☰
+                </button>
+                
+                <ul className={`navbar-nav ${isMenuOpen ? 'active' : ''}`}>
+                    <li className="nav-item">
+
+                        <Link to="/About" className="nav-link">About</Link>
+                    </li>
+                    { ( isMainPage ? (
+                            <li className="nav-item">
+                                <Link to="/register" className="nav-link">Register</Link>
+                            </li>
+                            
+                    ) : 
+                            <li className="nav-item">
+                                <Link to="/" className="nav-link">Home</Link>
+                            </li>
+                    )}
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+
+export default NavBar;
