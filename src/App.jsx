e4535e793baa72bc661447cfbd754930a9b3fb36
eric matutu <ericmatutu125@gmail.com>
2025-07-27 12:46:10 +0300

    feat: add About route to the application

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
 import Send from './pages/Send';
 import Register from './pages/Register';
+import About from './pages/About';
+// import NotFound from './pages/NotFound';
 
 
 import './App.css'; //main css
@@ -16,6 +18,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/send" element={<Send />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/about" element={<About />} />
         {/* <Route path="*" element={<NotFound />} />  */}
       </Routes>
     </Router>    
